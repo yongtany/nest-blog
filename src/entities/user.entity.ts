27463190d4a188c33fecad6bde720aa070dbc18c
yongtany <yongn1494@gmail.com>
2020-12-03 18:55:25 +0900

    add: article entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,9 +1,18 @@
-import { Entity, Column, BeforeInsert, JoinTable, ManyToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  BeforeInsert,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 import { Exclude, classToPlain } from 'class-transformer';
 import { IsEmail } from 'class-validator';
 
 import { AbstractEntity } from './abstract-entity';
+import { ArticleEntity } from './article.entity';
 
 @Entity('users')
 export class UserEntity extends AbstractEntity {
@@ -30,6 +39,13 @@ export class UserEntity extends AbstractEntity {
 
   @ManyToMany((type) => UserEntity, (user) => user.followers)
   followee: UserEntity[];
+
+  @OneToMany((type) => ArticleEntity, (article) => article.author)
+  articles: ArticleEntity[];
+
+  @ManyToMany((type) => ArticleEntity, (article) => article.favoritedBy)
+  @JoinColumn()
+  favorites: ArticleEntity[];
 
   @BeforeInsert()
   async hashPassword() {
